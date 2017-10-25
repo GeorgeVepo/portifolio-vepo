aef185472717c7448e1fa345214db528d3147f24
George Vepo <george.vepog@gmail.com>
2017-10-25 21:23:52 -0200

    modificado estrutura e nome de pastas e arquivos

--- portifolio-vepo/src/app/home/home.module.ts
+++ portifolio-vepo/src/app/home/home.module.ts
@@ -3,22 +3,18 @@ import { CommonModule } from '@angular/common'
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
-import { MainPostComponent } from '../common/main-post/main-post.component';
+import { ProjectComponent } from './project-list/project/project.component';
 import { HomeComponent } from './home.component';
-import { RecentPostComponent } from './recent-post/recent-post.component';
-import { PopularPostComponent } from './popular-post/popular-post.component';
-import { PostSelectionComponent } from './popular-post/post-selection/post-selection.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
     imports: [
         CommonModule
     ],
     declarations:[
-        MainPostComponent,
+        ProjectComponent,
         HomeComponent,
-        RecentPostComponent,
-        PopularPostComponent,
-        PostSelectionComponent
+        ProjectListComponent
     ],
     exports: [
         HomeComponent
