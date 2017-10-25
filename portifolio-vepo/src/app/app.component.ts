583ffa60c12a77eb263abc5fdfdacef3f3da22af
GeorgeVepo <george.vepog@gmail.com>
2017-10-25 18:52:20 -0200

    Modificado imagem principal e colocado efeito em imagens do painel

--- portifolio-vepo/src/app/app.component.ts
+++ portifolio-vepo/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RecentPostComponent } from './home/recent-post/recent-post.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  recentPostComponent: RecentPostComponent;
+
+  constructor() {
+   }
+
+  ngOnInit() {
+    this.recentPostComponent = new RecentPostComponent();
+  }
+
+
+  todos(){
+    this.recentPostComponent.todos();
+  }
+
+  angular(){
+    this.recentPostComponent.angular();
+  }
+
+  jsf(){
+    this.recentPostComponent.jsf();
+  }
+
+  android(){
+    this.recentPostComponent.android();
+  }
+
+  java(){
+    this.recentPostComponent.java();
+  }
+
+  typescript(){
+    this.recentPostComponent.typescript();
+  }
   title = 'app works!';
 }
--- portifolio-vepo/src/app/home/recent-post/recent-post.component.ts
+++ portifolio-vepo/src/app/home/recent-post/recent-post.component.ts
@@ -12,4 +12,34 @@ export class RecentPostComponent implements OnInit {
   ngOnInit() {
   }
 
+  todos(){
+    document.getElementById("taskmanager").setAttribute("class", "show-project");
+    document.getElementById("portifolio").setAttribute("class", "show-project");
+  }
+
+  angular(){
+    document.getElementById("taskmanager").setAttribute("class", "hide-project");
+    document.getElementById("portifolio").setAttribute("class", "show-project");
+  }
+
+  jsf(){
+    document.getElementById("taskmanager").setAttribute("class", "show-project");
+    document.getElementById("portifolio").setAttribute("class", "hide-project");
+  }
+
+  android(){
+    document.getElementById("taskmanager").setAttribute("class", "hide-project");
+    document.getElementById("portifolio").setAttribute("class", "hide-project");
+  }
+
+  java(){
+    document.getElementById("taskmanager").setAttribute("class", "show-project");
+    document.getElementById("portifolio").setAttribute("class", "hide-project");
+  }
+
+  typescript(){
+    document.getElementById("taskmanager").setAttribute("class", "hide-project");
+    document.getElementById("portifolio").setAttribute("class", "show-project");
+  }
+
 }
