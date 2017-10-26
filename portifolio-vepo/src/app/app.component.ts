fca682089abbeeb455077e0990d3a54f85b3a1c6
GeorgeVepo <george.vepog@gmail.com>
2017-10-26 14:31:04 -0200

    Revert "Merge de conflitos na estruturação de pastas e arquivos"
    This reverts commit c5ddeee456de80828de4021c912fc8b70c8bd2e7.

--- portifolio-vepo/src/app/app.component.ts
+++ portifolio-vepo/src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { RecentPostComponent } from './home/recent-post/recent-post.component';
 
 @Component({
   selector: 'app-root',
@@ -7,38 +6,12 @@ import { RecentPostComponent } from './home/recent-post/recent-post.component';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  recentPostComponent: RecentPostComponent;
 
   constructor() {
    }
 
   ngOnInit() {
-    this.recentPostComponent = new RecentPostComponent();
+
   }
 
-
-  todos(){
-    this.recentPostComponent.todos();
-  }
-
-  angular(){
-    this.recentPostComponent.angular();
-  }
-
-  jsf(){
-    this.recentPostComponent.jsf();
-  }
-
-  android(){
-    this.recentPostComponent.android();
-  }
-
-  java(){
-    this.recentPostComponent.java();
-  }
-
-  typescript(){
-    this.recentPostComponent.typescript();
-  }
-  title = 'app works!';
 }
--- portifolio-vepo/src/app/home/home.component.ts
+++ portifolio-vepo/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectListComponent } from '../home/project-list/project-list.component';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,35 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+   recentPostComponent: ProjectListComponent;
+
   ngOnInit() {
+    this.recentPostComponent = new ProjectListComponent();
+  }
+
+
+  todos(){
+    this.recentPostComponent.todos();
+  }
+
+  angular(){
+    this.recentPostComponent.angular();
+  }
+
+  jsf(){
+    this.recentPostComponent.jsf();
+  }
+
+  android(){
+    this.recentPostComponent.android();
+  }
+
+  java(){
+    this.recentPostComponent.java();
+  }
+
+  typescript(){
+    this.recentPostComponent.typescript();
   }
 
 }
